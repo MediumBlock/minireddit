6fb3e60ae0fd50591ee1591f0eab1493759d6883
Michael <m.paspaliaris@gmail.com>
2022-10-11 18:09:56 +1100

    updated basic tile structure

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,12 +1,23 @@
-import React, {useContext} from "react";
-import {Context} from "../Context"
+import React, { useContext } from "react";
+import { Context } from "../Context"
 import { mockData } from "../resources/data";
 
-export default function Tile({img, title}) {
+export default function Tile({ img, title }) {
     return (
         <div className="tile">
-            <p>{title}</p>
-            <img src={img} />
+            <div className="tile--divider">
+                <div className="tile--left">
+                    <p>hello</p>
+                </div>
+                <div className="tile--right">
+                    <p>{title}</p>
+                    <img src={img} />
+                    <div className="tile--footer">
+                        <p>harro</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
