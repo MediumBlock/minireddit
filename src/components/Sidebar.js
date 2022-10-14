021b4d5780e408e4c75441210f7822b62ac9876a
Michael <m.paspaliaris@gmail.com>
2022-10-14 18:24:32 +1100

    adding sidebar options and starting functionality

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,9 +3,23 @@ import React from "react";
 export default function SideBar() {
 
 
+    function handleChange(event) {
+        console.log(event.target.value)
+    }
+
     return (
         <div className="sidebar">
-            
-        </div>
+            <button value="worldnews"
+                onClick={handleChange}
+            >
+                worldnews
+            </button>
+            <button value="funny"
+                onClick={handleChange}
+            >
+                funny
+            </button >
+
+        </div >
     )
 }
